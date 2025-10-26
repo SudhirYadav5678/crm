36af253a967f86391e96ba4c0d2e1e27c69c3921
Sudhir Yadav <sudhirkyadav51@gmail.com>
2025-10-26 16:33:14 +0530

    add: userVerifiy middleware

--- src/middlewares/verfiyUserReq.middleware.js
+++ src/middlewares/verfiyUserReq.middleware.js
@@ -1,5 +1,22 @@
+import { User } from "../models/user.model";
 
 
-export const validateUserReq = (req, res, next) => {
+export const validateUserReq = async (req, res, next) => {
+    // if (req.body.name) {
+    //     res.status(400).send({
+    //         message: "Failed! Bad request, username not found"
+    //     })
+    // }
+    const { name, userId, email, password, userType } = req.body;
+
+    if (!name || !userId || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne(userId);
+    if (user != null) {
+        return res.status(400).send({
+            message: "User already exits"
+        })
+    }
 
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+export const userType = {
+    customer: "CUSTOMER",
+    engineer: "ENGINEER",
+    admin: "ADMIN"
+}
+
+export const userStatus = {
+    pending: "PENDING",
+    approved: "APPROVED",
+    blocked: "BLOCKED"
+}
