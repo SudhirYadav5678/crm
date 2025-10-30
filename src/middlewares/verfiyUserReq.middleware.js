c398063dc2903b43f33c441b60fc0b774525a7d0
Sudhir Yadav <sudhirkyadav51@gmail.com>
2025-10-30 21:35:08 +0530

    add: user vailidatin through middleware

--- src/middlewares/verfiyUserReq.middleware.js
+++ src/middlewares/verfiyUserReq.middleware.js
@@ -1,12 +1,7 @@
-import { User } from "../models/user.model";
-
+import { User } from "../models/user.model.js";
+import { userType } from "../utils/constant.js";
 
 export const validateUserReq = async (req, res, next) => {
-    // if (req.body.name) {
-    //     res.status(400).send({
-    //         message: "Failed! Bad request, username not found"
-    //     })
-    // }
     const { name, userId, email, password, userType } = req.body;
 
     if (!name || !userId || !email || !password) {
@@ -18,5 +13,16 @@ export const validateUserReq = async (req, res, next) => {
             message: "User already exits"
         })
     }
+
+    //validate user Type
+    const possibleUserType = [userType.customer, userType.engineer, userType.admin,];
+    if (userType && !possibleUserType.includes(userType)) {
+        return res.status(400).send({
+            message: "User Type is not vaild"
+        })
+    }
+
+    //pass to next function
+    next();
 
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { signup } from "../controller/auth.controller.js";
-
+import { validateUserReq } from "../middlewares/verfiyUserReq.middleware.js"
 const router = express.Router();
 
 // Define signup route
-router.post("/auth/signup", signup);
+router.post("/auth/signup", validateUserReq, signup);
 
 export default router;
